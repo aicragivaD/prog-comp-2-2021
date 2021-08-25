c08fc7d31041da7e16d4535dff0309cea6487415
Davi <davichagasgarcia@gmail.com>
2021-08-25 19:12:45 -0300

    25/08

---
+++ exer1.js
@@ -0,0 +1,19 @@
+let exercicio1 = () => {
+    //objetos
+    let vetorEmpresa = []
+    for (let i=0; i<4; i++){
+        let objeto = {
+            codigo: Number(prompt(`Informe o codigo do vendedor`)), //codigo do vendedor
+            venda: prompt(`Informe o total de vendas`), //cadastrar a venda
+            Mes: prompt(`Informe o mes`) //vendas do funcionario em determinado mes
+        }
+    }
+    //cadastro vendedor
+    let maisVendeu = 0
+    for (let i=0; i<4; i++){
+        if (codigo){
+            
+        }
+
+    }
+}
