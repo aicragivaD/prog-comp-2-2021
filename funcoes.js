039a4bdb00b27271a3513cb15f9acbe6ce8a9302
aicragivaD <davichagasgarcia@gmail.com>
2021-09-08 20:14:02 -0300

    08/09

--- funcoes.js
+++ funcoes.js
@@ -1,13 +1,18 @@
 function principal(){
 
+    let opcao
+    let idade //escopo local, funciona no local onde foi definido
     do{
         opcao = Number(prompt(`Digite: \n1. F1 \n2. F2 \n3. F3 \n4. Sair`))
         switch(opcao){
-            case 1: f1()
+            case 1: idade = Number(prompt(`Informe a sua idade`))
+                f1(idade)
                 break
-            case 2: f2()
+            case 2: idade = Number(prompt(`Informe a sua idade`))
+                f2(idade)
                 break
-            case 3: f3()
+            case 3: idade = Number(prompt(`Informe a sua idade`))
+                f3(idade)
                 break
             case 4: alert(`Programa Encerrado!`)
                 break
@@ -18,15 +23,16 @@ function principal(){
 }
 
 
-function f1(){
-    alert(`Chamada de F1`)
+function f1(idade){
+    //let idade = 10
+    alert(`Valor de idade ${idade}`)
 
 }
-function f2(){
-    alert(`Chamada de F2`)
+function f2(idade){
+    alert(`Valor de idade ${idade}`)
 
 }
-function f3(){
-    alert(`Chamada de F3`)
+function f3(idade){
+    alert(`Valor de idade ${idade}`)
 
 }
