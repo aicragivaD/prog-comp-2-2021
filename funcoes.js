9b0e0cee364537a65c781b94fab9327f587b789f
aicragivaD <davichagasgarcia@gmail.com>
2021-09-08 19:59:10 -0300

    08/09

--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,22 @@
 function principal(){
 
-    f1()
-    F2()
-    f3()
+    do{
+        opcao = Number(prompt(`Digite: \n1. F1 \n2. F2 \n3. F3 \n4. Sair`))
+        switch(opcao){
+            case 1: f1()
+                break
+            case 2: f2()
+                break
+            case 3: f3()
+                break
+            case 4: alert(`Programa Encerrado!`)
+                break
+            default: alert(`Opção Inválida!`)
+        }
+    }
+    while (opcao!= 4)
 }
+
 
 function f1(){
     alert(`Chamada de F1`)
