1613f511242d47464e1eee6eb9e81ac7bb12e470
aicragivaD <davichagasgarcia@gmail.com>
2021-09-08 19:43:25 -0300

    08/09

---
+++ funcões.js
@@ -0,0 +1,19 @@
+function principal(){
+
+    f1()
+    F2()
+    f3()
+}
+
+function f1(){
+    alert(`Chamada de F1`)
+
+}
+function f2(){
+    alert(`Chamada de F2`)
+
+}
+function f3(){
+    alert(`Chamada de F3`)
+
+}
