338da77a9587db51793cb4db5d418cd837bcf835
aicragivaD <davichagasgarcia@gmail.com>
2021-09-08 20:20:34 -0300

    08/09

--- funcoes.js
+++ funcoes.js
@@ -2,18 +2,22 @@ function principal(){
 
     let opcao
     let idade //escopo local, funciona no local onde foi definido
+    let peso, altura
     do{
         opcao = Number(prompt(`Digite: \n1. F1 \n2. F2 \n3. F3 \n4. Sair`))
         switch(opcao){
             case 1: idade = Number(prompt(`Informe a sua idade`))
-                f1(idade)
-                break
+                   f1(idade)
+                   break
             case 2: idade = Number(prompt(`Informe a sua idade`))
-                f2(idade)
-                break
+                    peso = Number(prompt(`Informe seu peso`))
+                    f2(idade, peso)
+                    break
             case 3: idade = Number(prompt(`Informe a sua idade`))
-                f3(idade)
-                break
+                    peso = Number(prompt(`Informe seu peso`))
+                    altura = Number(prompt(`Informe sua altura`))
+                    f3(idade, peso, altura)
+                    break
             case 4: alert(`Programa Encerrado!`)
                 break
             default: alert(`Opção Inválida!`)
@@ -28,11 +32,11 @@ function f1(idade){
     alert(`Valor de idade ${idade}`)
 
 }
-function f2(idade){
-    alert(`Valor de idade ${idade}`)
+function f2(idade, peso){
+    alert(`Valor de idade ${idade} e do peso ${peso}`)
 
 }
-function f3(idade){
-    alert(`Valor de idade ${idade}`)
+function f3(idade, peso, altura){
+    alert(`Valor de idade ${idade}, peso ${peso} e altura ${altura}`)
 
 }
