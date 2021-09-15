7aecb0e065b00e6d12de81a08a74341a226fb617
aicragivaD <davichagasgarcia@gmail.com>
2021-09-14 22:23:58 -0300

    14/09

--- funcoes.js
+++ funcoes.js
@@ -3,8 +3,9 @@ function principal(){
     let opcao
     let idade //escopo local, funciona no local onde foi definido
     let peso, altura
+    let vetor = [] //declaração de vetor
     do{
-        opcao = Number(prompt(`Digite: \n1. F1 \n2. F2 \n3. F3 \n4. Sair`))
+        opcao = Number(prompt(`Digite: \n1. F1 \n2. F2 \n3. F3 \n4. F4 \n5. Sair`))
         switch(opcao){
             case 1: idade = Number(prompt(`Informe a sua idade`))
                    idade = f1(idade)
@@ -18,8 +19,15 @@ function principal(){
                     altura = Number(prompt(`Informe sua altura`))
                     f3(idade, peso, altura)
                     break
-            case 4: alert(`Programa Encerrado!`)
-                break
+            case 4: vetor.push(1)
+                    vetor.push(2)
+                    vetor.push(3)
+                    f4(vetor) //passando um vetor como paremetro de uma funcao
+                    //passar um vetor como parametro e passar uma referencia do vetor
+                    alert(vetor)
+                    break
+            case 5: alert(`Programa Encerrado!`)
+                    break
             default: alert(`Opção Inválida!`)
         }
     }
@@ -42,5 +50,13 @@ function f2(idade, peso){
 //funcao que recebe parametro
 function f3(idade, peso, altura){
     alert(`Valor de idade ${idade}, peso ${peso} e altura ${altura}`)
+
+}
+
+function f4(vetor){
+    //alterando o valor do vetor
+    vetor [0] = vetor[0] + 10
+    vetor [1] = vetor[1] + 10
+    vetor [2] = vetor[2] + 10
 
 }
